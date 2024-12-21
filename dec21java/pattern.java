a49035d2d38bcc99a2c6440252e3a48d824ba20d
gaddamakhil8 <22bk1a7240@stpetershyd.com>
2024-12-21 11:14:24 +0530

    first commit

---
+++ dec21java/pattern.java
@@ -0,0 +1,20 @@
+package dec21java;
+
+public class pattern {
+
+	public static void main(String[] args) {
+
+
+		
+		for(int r=1,n=1;r<=5;r++) {
+			for(int c=1;c<=r;c++) {
+				System.out.print(n++ +" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+	
+
+
